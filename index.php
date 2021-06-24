4d5c2f9948bde4cccfc0207d2fb80c9f6cf73d32
rshersten <roman@shersten.com>
2021-06-24 23:25:31 +0300

    test commit

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html> 
 	<head> 
-	<title>PHP Test</title>
+	<title>PHP Roman's Test</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
-	<h1>PHP Test</h1>
+	<h1>PHP Roman's Test</h1>
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
